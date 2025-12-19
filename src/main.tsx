08d9338682bcb77041785a385b1b17445f95fa82
minji <minji212657@gmail.com>
2025-12-20 00:04:47 +0900

    1차 GA 연동 시 추가 진행 내용 요약
    1. npm install react-ga4 로 ga4가져옴
    2. ".env"파일 구글 애널리 틱스 측정 ID 값
    3. "main.tsx" 파일  GA_ID 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './App'
 import './index.css'
+
+const GA_ID = import.meta.env.VITE_GA_MEASUREMENT_ID
+if (GA_ID) {
+  ReactGA.initialize(GA_ID)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
